22b3af949acb3dd3547b91fa3052f32ec036b171
Cupofme <danil.ilyashuk@gmail.com>
2018-10-19 23:26:55 +0300

    player choose page
    - Нчата работа над страницей выбора количетва игроков

--- app/src/main/java/carcassonne/se/carcassonnecustomclone/PlayersActivity.kt
+++ app/src/main/java/carcassonne/se/carcassonnecustomclone/PlayersActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_players.*
 
 class PlayersActivity : AppCompatActivity() {

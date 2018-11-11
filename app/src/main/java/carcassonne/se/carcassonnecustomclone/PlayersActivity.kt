01d4aa3505172eafe70a5b2b68e2f68ee17c8881
Cupofme <danil.ilyashuk@gmail.com>
2018-11-11 22:04:40 +0300

    player activiry update
    - Изменена структура страницы.
    - Частично реализована возможность изменения имени игрока

--- app/src/main/java/carcassonne/se/carcassonnecustomclone/PlayersActivity.kt
+++ app/src/main/java/carcassonne/se/carcassonnecustomclone/PlayersActivity.kt
@@ -1,14 +1,25 @@
 package carcassonne.se.carcassonnecustomclone
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.view.View
+import android.widget.EditText
+
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
+
 import kotlinx.android.synthetic.main.activity_players.*
 import java.util.*
 
@@ -60,35 +71,62 @@ class PlayersActivity : AppCompatActivity() {
 
     /*Добавление нового игрока*/
     private fun addNewPlayer() {
-        playerArea.removeView(findViewById(R.id.addPlayerButton))
+        playerIcons.removeView(findViewById(R.id.addPlayerButton))
         addPlayerButton()
         setFullscreenMode()
         addAddButton()
+        addPlayerName()
     }
 
-    /*Добавляет кружок игрока в список*/
-    fun addPlayerButton() {
-        val newPlayer = ImageButton(this)
+    private fun addPlayerName() {
+        val newPlayerName = TextView(this, null, R.style.PlayerName)
         val params = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
         params.setMargins(10, 10, 10, 10)
-        newPlayer.layoutParams = params
-        newPlayer.setImageResource(R.drawable.ic_player)
-        newPlayer.setBackgroundResource(R.drawable.circle)
-        val arr = resources.getStringArray(R.array.PlayerColors)
-        (newPlayer.background as? GradientDrawable)?.setColor(Color.parseColor(resources.getStringArray(R.array.PlayerColors)[players.size]))
-        newPlayer.setOnClickListener {
+        params.width = 300 //TODO: ширина
+        newPlayerName.layoutParams = params
+        newPlayerName.gravity = Gravity.CENTER_HORIZONTAL
+        newPlayerName.text = players[players.size - 1]
+        //TODO: сделать нормальный диалог
+        newPlayerName.setOnClickListener {
+            val editText = EditText(this)
+            editText.setText("")
+            val alert = AlertDialog.Builder(this)
+            alert.setView(editText)
+            alert.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->  })
+            alert.setNegativeButton("OK", DialogInterface.OnClickListener { dialog, which ->  })
+            alert.setTitle("Change player name")
+            alert.show()
+        }
+        playerNames.addView(newPlayerName)
+    }
+
+    //TODO: пофиксить выбор цветов
+
+    /*Добавляет иконку игрока в список*/
+    fun addPlayerButton() {
+        val newPlayerIcon = ImageButton(this)
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        params.setMargins(10, 10, 10, 10)
+        newPlayerIcon.layoutParams = params
+        newPlayerIcon.setImageResource(R.drawable.ic_player)
+        newPlayerIcon.setBackgroundResource(R.drawable.circle)
+        (newPlayerIcon.background as? GradientDrawable)?.setColor(Color.parseColor(resources.getStringArray(R.array.PlayerColors)[players.size]))
+        newPlayerIcon.setOnClickListener {
             if (players.size > 2) {
                 players.removeAt(players.lastIndex)
-                playerArea.removeView(newPlayer)
+                playerIcons.removeView(newPlayerIcon)
                 if (players.size == 5) {
                     addAddButton()
                 }
             }
         }
-        playerArea.addView(newPlayer)
+        playerIcons.addView(newPlayerIcon)
         players.add("Player${players.size + 1}") // TODO: нормально собирать инфу и отправлять ее
     }
 
@@ -115,7 +153,7 @@ class PlayersActivity : AppCompatActivity() {
             newAddButton.setOnClickListener {
                 addNewPlayer()
             }
-            playerArea.addView(newAddButton)
+            playerIcons.addView(newAddButton)
         }
     }
 }
